279dcc530d33d8ec83b937891ee45447a40341a6
JChappieT <jerochapa2025@gmail.com>
2025-02-27 10:03:59 -0500

    clase 27/02
    creacion de programas en la clase

---
+++ Ejercicios/clase27_02-1.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h> //de aqui viene la funcion printf
+
+#define size 10
+int main(int argc, char *argv[]){
+
+    int *vectorPunteros[3];
+    int p=49, q=80, r=120;
+    vectorPunteros[0]= &p;
+    vectorPunteros[1]= &q;
+    vectorPunteros[2]= &r;
+
+    printf("\nForma de acceso al vector de punteros: \tValores:\n");
+    for(int i=0; i<3; i++){
+        printf("Para la direccion %p \t%d\n", vectorPunteros[i], *vectorPunteros[i]);
+    }
+    return 0;
+
+}
---
+++ Ejercicios/clase27_02.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h> //de aqui viene la funcion printf
+
+int main(int argc, char *argv[]){
+
+    int variable = 123;
+    int *ptr = &variable;
+    printf("Valor de la variable %d \n", variable); //%d es para enteros, si no lo pongo no muestra el valor
+    printf("Direccion de memoria de la variable %p \n", &variable); //%p es para direccion de memoria
+    printf("Direccion a la que apunta el puntero %p \n", (void*)ptr);
+    printf("Valor de la variable %i \n", *ptr);
+
+    return 0;
+
+}
