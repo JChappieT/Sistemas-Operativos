efc2ff3e0faea466049a1a07eebcfd96f1dbfe02
JChappieT <jerochapa2025@gmail.com>
2025-03-20 11:45:03 -0500

    Create Chaparro_Fork.c

---
+++ Clases/Clase20032025/Chaparro_Fork.c
@@ -0,0 +1,19 @@
+/***********************************************************************
+ Fecha 20 03 2025
+   Autor: Jeronimo Chaparro Tenorio
+   Tema: Creación de procesos con FORK
+   Materia: Sistemas Operativos
+   PONTIFICIA UNIVERSIDAD JAVERIANA
+***********************************************************************/
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(int argc, char *argv[]){
+    int x;
+    for(x=0; x<1; x++){
+        fork();
+        printf("The proces IDD (PID): %d \n", getpid());
+    }
+    return 0;
+}
