046e2fccd798262db8bc52651771be3fafa4085c
JChappieT <jerochapa2025@gmail.com>
2025-03-27 11:55:41 -0500

    Update ejercicio2.c

--- Clases/Clase27032025/ejercicio2.c
+++ Clases/Clase27032025/ejercicio2.c
@@ -16,22 +16,28 @@
 
 void *funcion(void *arg){
     printf("FUNCION HILO: INICIO\n");
-    sleep(2);  
-    size_t job = *(size_t*)arg;
-    printf("Job %zu\n", job);
+    sleep(2); //ESPERA 2 SEGUNDOS (dormir)
+    printf("FUNCION HILO: FIN\n");
     return NULL;
 }
 
-int main(int argc, char *argv[]){
-    size_t jobs[N];     //Vector de trabajos
-    pthread_t hilos[N]; //Vector de hilos
-    for(size_t i=0; i<N; i++){
-        jobs[i]= i;
-        pthread_create(&hilos[i], NULL, funcion, jobs+i);
+int main(int argc, char **argv[]){
+    pthread_t hilo; //se identifica el hilo
+    //Se crea el hilo, se envia la función
+    int hiloCreacion = pthread_create(&hilo, NULL, &funcion, NULL);
+    //Se verifica que el hilo se creo exitosamente
+    if(hiloCreacion){
+        printf("fallo de creacion de hilo :: %d\n", hiloCreacion);
+    }else{
+        printf("Hilo creado con id :: %d\n", hiloCreacion);
     }
-    for(size_t i=0; i<N; i++){
-        pthread_join(hilos[i], NULL);//Se espera a que cada hilo finalice
+    printf("Espera salida del hilo.....\n");
+    hiloCreacion = pthread_join(hilo, NULL);//Se espera a que el proceso del hilo finalice
+    //Se verifica sale exitosamente
+    if(hiloCreacion){
+        printf("fallo de creacion de hilo :: %d\n", hiloCreacion);
     }
+    printf("Final del programa principal.\n");
     return 0;
 
 }
